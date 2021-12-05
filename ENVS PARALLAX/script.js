ced589bf730a6d02e8c0e93cca563c864f8f4501
sdzyacky1 <95240195+sdzyacky1@users.noreply.github.com>
2021-12-05 13:14:12 -0600

    Add files via upload

---
+++ ENVS PARALLAX/script.js
@@ -0,0 +1,12 @@
+function getSelectedCheckboxValues(name) {
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    return values;
+}
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) => {
+    alert(getSelectedCheckboxValues("Recycle"));
+});
